b59b42ca741b9ab2ac595204fb9918dc974eb81a
leosouzaaaaa <souza.leonardo26@escola.pr.gov.br>
2024-09-19 08:08:08 -0300

    dfghdfghdfg

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'Biologia',
+    'Sobre a teoria da evolução de Darwin, o que ele não conseguiu demontrar em sua pesquisa?',
+    'A origem das variações entre os indivíduos.')
+
+criaCartao(
+    'Biologia',
+    'Qual a função da cartilgem elástica?',
+    'As principais funções são: sustentação e flexibilidade. ')
+
+criaCartao(
+    'Química',
+    'Qual a função dos ácidos carboxilicos?',
+    'Produção do vinagre; Produção da seda artificial; Produção de desinfetantes; Tingimento de tecidos.')
+
+criaCartao(
+    'Química',
+    'A molécula do OF2 é polar e a molécula do BeF2 é apolar. Isto se deve à (ao):',
+    'Geometria molecular')
+
+criaCartao(
+    'Física',
+    'Qual a definição de física óptica?',
+    'Área da física que estuda os fenômenos relacionados à luz')
+
+criaCartao(
+    'Física',
+    'O que é a Lei de Coulomb?',
+    'Estabelece que a força eletrostática entre duas cargas elétricas é proporcional ao módulo das cargas elétricas e inversamente proporcional ao quadrado da distância que as separa.')
+
+criaCartao(
+    'Geografia',
+    'Quais as divisões geoeconômicas do Brasil?',
+    'Amazônia, o Nordeste e o Centro-Sul."
+
+criaCartao(
+    'Geografia',
+    'Quais os prinicipais blocos econômicos? ',
+    'União Europeia, Nafta, Opep, Apec, MCCA, Comunidade Andina, Mercosul, CEI, SADC')
+
+criaCartao(
+    'História',
+    'O que é o Apothetai?',
+    '“Local de exposição” onde os bebês espartanos eram supostamente deixados para morrer se fossem considerados inadequados')
+
+criaCartao(
+    'História',
+    '        ',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta')
