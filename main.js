376499c7a086b68af3e72203a43c4a92b616eb56
leosouzaaaaa <souza.leonardo26@escola.pr.gov.br>
2024-09-30 08:33:47 -0300

    xhxgh

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ function criaCartao (categoria, pergunta, resposta){
     cartao.className = 'cartao';
     cartao.innerHTML = `<div class="conteudo-cartao">
    <h3>${categoria}</h3>
-   </div>
+
    <div class="pergunta-cartao">
     <p>${pergunta}</p>
     </div>
    <div class="resposta-cartao">
   <p>${resposta}</p>
    </div>
-  </div>`
+   </div></div>`
 
   let respostaVisivel= false;
 function viraCartao(){
