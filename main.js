2049df5e2e3ea0a32a555d7bb02576dcc2134b5f
leosouzaaaaa <souza.leonardo26@escola.pr.gov.br>
2024-09-30 07:32:03 -0300

    dffghh

---
+++ main.js
@@ -0,0 +1,23 @@
+function Criacartão (categoria,pergunta,resposta){
+    let container = document.getElementById(container);
+    let cartao = document.createElement('article');
+    cartao.className = 'RodrigoColorado';
+    cartao.innerHTML = `<div class="conteudo-cartao">
+   <h3>${categoria}</h3>
+   <div class="pergunta-cartao">
+    <p>${pergunta}</p>
+    </div>
+   <div class="resposta-cartao">
+  <p>${resposta}</p>
+   </div>
+  </div>`
+
+  let respostaVisivel= false;
+function viraCartao(){
+respostaVisivel=!respostaVisivel;
+cartao.classList.toggle('active', respostaVisivel);
+}
+cartao.addEventListener('click', viraCartao);
+container.appendChild(cartao);
+}
+
